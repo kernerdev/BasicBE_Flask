92c252588247d17e2aacf7b87900d9df75b060d9
Bence Kerner <bence.kerner@continental-corporation.com>
2020-10-17 21:34:15 +0200

    init app

---
+++ test/basic_test.py
@@ -0,0 +1,14 @@
+import unittest
+from src.main import app
+
+class BasicTestCase(unittest.TestCase):
+    
+    def test_basic(self):
+            tester = app.test_client(self)
+            response = tester.get('/hello', content_type='application/json')
+            print(response)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data, b'{"message":"Hello, World!"}')
+
+if __name__ == '__main__':
+    unittest.main()
